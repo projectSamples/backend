e531bf849252760dc876247a02771ed23d0c1be1
projectSamples <kinnera.mandhapati@gmail.com>
2020-05-26 17:28:45 +0530

    test commit

--- src/main/java/com/springboot/h2/ctrl/EmployeeController.java
+++ src/main/java/com/springboot/h2/ctrl/EmployeeController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,8 @@ import com.springboot.h2.serv.ProjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@RestController // Useful to create the RESTful webservices.
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 
 @Api(value = "/dashboard", description = "dashboard", produces = "application/json")
 @RequestMapping("/dashboard")
